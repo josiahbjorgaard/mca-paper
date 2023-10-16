02bb71a89014c75dbc73559c09b307307094b9ed
Josiah Bjorgaard <josiahbjorgaard@gmail.com>
2023-10-16 21:27:30 +0000

    Add gpu training script

--- train_gpu.py
+++ train_gpu.py
@@ -23,14 +23,11 @@ from yacs.config import CfgNode as CN
 import wandb 
 
 config = CN()
-config.epochs = 100
-config.freeze_layers=6
-config.batch_size = 2
+config.epochs = 10
+config.batch_size = 16
 config.warmup_steps = 1000
 config.lr = 1e-4
-config.dropout = 0.3
 config.output_dir = "test_output1"
-config.num_labels = 1
 config.hidden_size = 512
 config.dataset = "/efs-private/multimodal/data/filtered_protein_mrna_genes"
 
@@ -40,16 +37,18 @@ logger = logging.getLogger(__name__)
 torch.manual_seed(0)
 
 ## define xla as device for using AWS Trainium Neuron Cores
-device = "cpu"
+device = "cuda"
 
-torch.distributed.init_process_group(device)
-
+#torch.distributed.init_process_group(device)
+torch.distributed.init_process_group()
 # Get the global number of workes.
 #world_size = xm.xrt_world_size()
+world_size = torch.distributed.get_world_size()
 logger.info("Workers: {}".format(world_size))
 logger.info("Device: {}".format(device))
 
 lm_datasets = load_from_disk(config.dataset).with_format('torch')
+
 keep =  ['spliced_index', 'unspliced_index', 'spliced_data', 'unspliced_data']
 remove = list()
 for key in lm_datasets.features.keys():
@@ -59,8 +58,9 @@ lm_datasets = lm_datasets.remove_columns(remove)
 lm_datasets = lm_datasets.train_test_split(0.1)
 
 #BioZorro Collator
-default_data_collator = BioZorroCollator(pad_len=2048, pad_token=0)
+default_data_collator = BioZorroCollator(pad_len=1024, pad_token=0)
 
+print(f"Number of training samples: {len(lm_datasets['train'])}")
 
 if __name__ == '__main__':
     path = os.path.abspath("data")
@@ -80,7 +80,8 @@ if __name__ == '__main__':
             rank=xm.get_ordinal(),
             shuffle=True,
         )
-
+    else:
+        train_sampler = None
     ## Creating a DataLoader object for iterating over it during the training epochs
     train_dl = DataLoader(
         lm_datasets["train"],
@@ -89,10 +90,11 @@ if __name__ == '__main__':
         sampler=train_sampler,
         shuffle=False if train_sampler else True)
 
+    sample = next(iter(train_dl))
+    [print(f"{k}:{v.shape}") for k,v in sample.items()]
+    print(f"Number of batches: {len(train_dl)}")
     ## Loading a subset of the data in the different Neuron Cores provided as input
 #    train_device_loader = pl.MpDeviceLoader(train_dl, device)
-
-
 
     #### MODEL
     model_config = {
@@ -128,7 +130,7 @@ if __name__ == '__main__':
     ## Start model training and defining the training loop
     model.train()
     for epoch in range(config.epochs):
-        for batch in train_device_loader:
+        for batch in train_dl:
             batch = {k: v.to(device) for k, v in batch.items()}
             outputs = model(**batch)
             optimizer.zero_grad()
@@ -138,10 +140,11 @@ if __name__ == '__main__':
  #           xm.optimizer_step(optimizer)
             optimizer.step()
             progress_bar.update(1)
+            wandb.log({"loss":loss.detach().to("cpu")})
             #
         #if xm.is_master_ordinal(local=False):
-        wandb.log({"epoch_loss":loss.detach().to("cpu")})
-        logger.info("Epoch {}, rank {}, Loss {:0.4f}".format(epoch, xm.get_ordinal(), loss.detach().to("cpu")))
+        #wandb.log({"epoch_loss":loss.detach().to("cpu")})
+        #logger.info("Epoch {}, rank {}, Loss {:0.4f}".format(epoch, xm.get_ordinal(), loss.detach().to("cpu")))
 
     logger.info("End training: {}".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 
